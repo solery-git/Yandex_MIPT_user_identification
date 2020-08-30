988a503d5eee9bf84d85bd7a6a0d5179cee6a940
Alex Galak <aleksey.galak@gmail.com>
2020-08-30 06:01:06 +0300

    Add submission file creation via parameters

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -12,8 +12,19 @@ from sklearn.metrics import roc_auc_score
 PROJECT_DIR = Path(__file__).resolve().parents[2]
 PATH_PROCESSED = 'data/processed'
 PATH_MODELS = 'models'
-SEED = yaml.safe_load(open(PROJECT_DIR.joinpath('params.yaml')))['meta']['seed']
+PATH_SUBMISSIONS = 'kaggle_submissions'
+PARAMS_ALL = yaml.safe_load(open(PROJECT_DIR.joinpath('params.yaml')))
+SEED = PARAMS_ALL['meta']['seed']
+PARAMS = PARAMS_ALL['evaluate']
 
+
+def write_to_submission_file(predicted_labels, out_file,
+                             target='target', index_label="session_id"):
+    # turn predictions into data frame and save as csv file
+    predicted_df = pd.DataFrame(predicted_labels,
+                                index = np.arange(1, predicted_labels.shape[0] + 1),
+                                columns=[target])
+    predicted_df.to_csv(out_file, index_label=index_label)
 
 def main():
     with open(PROJECT_DIR.joinpath(PATH_PROCESSED, 'X_train.pkl'), 'rb') as fin:
@@ -45,6 +56,11 @@ def main():
     logit.fit(X_train, y_train)
     logit_valid_score = roc_auc_score(y_valid, logit.predict_proba(X_valid)[:, 1])
     metrics['valid'] = float(logit_valid_score)
+    
+    if PARAMS['submission']['make']:
+        logit_test_proba = logit.predict_proba(X_test_sparse)[:, 1]
+        out_file = PARAMS['submission']['name'] + '.csv'
+        write_to_submission_file(logit_test_proba, PROJECT_DIR.joinpath(PATH_SUBMISSIONS, out_file))
     
     with open(PROJECT_DIR.joinpath(PATH_MODELS, 'metrics.yaml'), 'w') as fout:
         yaml.dump(metrics, fout)
